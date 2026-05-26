05e1a95f8c9599b04d1eb0180ee129712d9e8749
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-05-26 18:09:58 +0800

    resolve merge conflict in routes

--- routes/web.php
+++ routes/web.php
@@ -4,16 +4,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HRISReportController;
 use App\Http\Controllers\ReportController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/reports/daily', [ReportController::class, 'daily']);
 Route::get('/reports/late', [ReportController::class, 'late']);
