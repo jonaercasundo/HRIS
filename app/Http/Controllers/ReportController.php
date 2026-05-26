39698f2e19e339557131876d47c5e39a073c683a
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-05-26 17:12:36 +0800

    update code 05262026

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -111,7 +111,7 @@ class ReportController extends Controller
             ->havingRaw('time_out IS NULL OR time_out = ""')
             ->get();
 
-        return view('reports.notimeout', [
+        return view('reports.no_time_out', [
             'data' => $data,
             'date' => $date
         ]);
