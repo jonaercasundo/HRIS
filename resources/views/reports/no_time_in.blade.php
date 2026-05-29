1770354b41262355ab4b2ef960292cf0999b0bd0
Jonaer Casundo <jcasundo.sedge@gmail.com>
2026-05-29 09:14:33 +0800

    new update for time-in columns

--- resources/views/reports/no_time_in.blade.php
+++ resources/views/reports/no_time_in.blade.php
@@ -66,7 +66,7 @@
             </tr>
         @empty
             <tr>
-                <td colspan="4" class="text-center">No records found</td>
+                <td colspan="6" class="text-center">No records found</td>
             </tr>
         @endforelse
     </tbody>
